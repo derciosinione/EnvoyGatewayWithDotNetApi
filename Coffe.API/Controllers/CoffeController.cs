7726de39655937b4e925043bd2e2aa03ed28327c
derciosinione <derciosinione@gmail.com>
2021-10-27 19:03:09 +0100

    Setup Projects

---
+++ Coffe.API/Controllers/CoffeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Coffe.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeController : ControllerBase
+    {
+        private static readonly string[] Coffees = new[]
+        {
+            "Flat White", "Long Black", "Latte", "Americano", "Cappuccino"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public CoffeController(ILogger<CoffeController> logger)
+        {
+            _logger = logger;
+        }
+    
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var rng = new Random();
+            
+            return Ok(Coffees[rng.Next(Coffees.Length)]);
+        }
+    }
+}
